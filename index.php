0803a5bf58fb560ca596175c844dce3dedff4559
Jared <Jared@172.25.25.32>
2016-06-23 10:03:29 -0700

    Jared - clean out original file

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- * Camden is testing this out
- */
- 
  <!DOCTYPE HTML>
 <html lang="en-us">
 <head>
