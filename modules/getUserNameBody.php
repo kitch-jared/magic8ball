03e940bebc0b284d0067fd94388b413f1eb05d2d
Jared <Jared@172.25.25.32>
2016-06-30 10:00:25 -0700

    Jared - fix a typo...

--- modules/getUserNameBody.php
+++ modules/getUserNameBody.php
@@ -3,7 +3,7 @@
             <form>
                 Welcome to the Magic 8 Ball where all of your questions may be answered.<br>
                 <br>
-                Please enter your name below so that we might better server you:<br>
+                Please enter your name below so that we might better serve you:<br>
                 <input type="text" id="name"<br>
                 <input type="submit" value="Save my name" onclick="saveUserName()">
             </form>
