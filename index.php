c2de24bd2b2266e08a983d721940c2b8d68b5fbc
Jared <Jared@172.25.25.32>
2016-07-07 13:50:49 -0700

    Jared - Cleaned up quotation marks

--- index.php
+++ index.php
@@ -106,10 +106,10 @@
     <div id="userNamePromptOverlay">
       <div id="userNamePrompt">
         <form>
-          "Welcome to the Magic 8 Ball where all of your questions may be answered."
+          Welcome to the Magic 8 Ball where all of your questions may be answered.
           <br>
           <br>
-          "Please enter your name below so that we might better serve you:"
+          Please enter your name below so that we might better serve you:
           <br>
           <input type="text" id="name">
           <br>
