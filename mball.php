791b96840b2981d83a4967b16c96733bbb8bd5a5
Prestwich-Maxine <toi12001@byui.edu>
2016-07-17 16:24:01 -0700

    This is what AJ and I...
    were trying to figure out last night. It's linked to ans.json.

--- mball.php
+++ mball.php
@@ -4,23 +4,23 @@
       <title>Magic 8 Ball</title>
       <script type = "application/javascript">
          function loadJSON(){
-            var url = "reply.json";
+            var url = "ans.json";
             var http_request = new XMLHttpRequest();
             http_request.onreadystatechange = function(){
                 if (http_request.readyState == 4 && http_request.status == 200){
                   // Javascript function JSON.parse to parse JSON data
                   var jsonObj = JSON.parse(http_request.responseText);
-
-                  // jsonObj variable now contains the data structure and can
-		  var ans = '';
-                    for (var i=0; i < jsonObj.length; i++){
+                  // jsonObj variable now contains the data structure
+		  var reply = '';
+                  var i;
+                    for (i=0; i < jsonObj.length; i++){
                     ans = jsonObj[i].answer;
                     }
-		    document.getElementById("answers").innerHTML = ans;
+		    document.getElementById("answers").innerHTML = reply;
 		    }
-                }
-            http_request.open("GET", url, true);
-            ajaxhttp.setRequestHeader("content-type","application/json");
+                };
+            http_request.open("GET", url + Math.random(), true);
+            http_request.setRequestHeader("content-type","application/json");
             http_request.send();
         }
 		
@@ -29,12 +29,11 @@
    </head>
 	
    <body>
-      <p id="text">This is a Magic 8 Ball!</p>
+      <p>This is a Magic 8 Ball!</p>
 	  <p>Ask a 'Yes' or 'No' question!<br></p>
  
- <button type="button" onclick='loadJson()'>Ask A Question</button><br>
- <div id='answers'></div>
-		
+ <button type="button" onclick='loadJSON()'>Ask A Question</button><br>
+ <div id="answers"></div>
    </body>
 		
 </html>
